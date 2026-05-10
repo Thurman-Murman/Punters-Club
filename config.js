331f5523aae779250ea89e7132b784c8b49dfb09
Thurman-Murman <134827689+Thurman-Murman@users.noreply.github.com>
2026-05-10 16:09:37 +1000

    Update config.js — Week 18 (2026-05-10)

--- config.js
+++ config.js
@@ -56,7 +56,8 @@ const CONFIG = {
     { bet: "8-Leg Multi: Lions H2H (vs North), Melbourne H2H (vs Essendon), Hawthorn +32.5 (vs Bulldogs), Geelong SGM (Cats H2H, Holmes 25+ Disp, Smith 25+ Disp), GWS H2H (vs Richmond), St Kilda H2H (vs Port)", odds: 4.65, result: "loss" },
     { bet: "4-Leg Multi: Plaintiff Win (Randwick R4), Angel Capital PLC (Randwick R9), Balance The Books Win (Eagle Farm R2), Olympian PLC (Randwick R5)", odds: 9.5, result: "loss" },
     { bet: "5-Leg Multi: Mcwoody Win (Morphettville R3), Silvasista Win (Flemington R6), Geelong H2H (vs Port), Fremantle H2H (vs Carlton), St Kilda H2H (vs West Coast)", odds: 10.59, result: "loss" },
-    { bet: "7-Leg Multi: Pistons H2H (vs Magic), Cavaliers H2H (vs Raptors), Lakers H2H (vs Rockets), Lions H2H (vs Essendon), West Coast H2H (vs Richmond), Geelong H2H (vs North), St Kilda H2H (vs Carlton)", odds: 18.81, result: "loss" }
+    { bet: "7-Leg Multi: Pistons H2H (vs Magic), Cavaliers H2H (vs Raptors), Lakers H2H (vs Rockets), Lions H2H (vs Essendon), West Coast H2H (vs Richmond), Geelong H2H (vs North), St Kilda H2H (vs Carlton)", odds: 18.81, result: "loss" },
+    { bet: "Hobart Quaddie: R5 (5,8,10,11), R6 (2,7,8,11), R7 (1,4,6,8), R8 (1,2,4,6) $25 Flexi 9.7% + Antisocial Win (Ballarat R5) $25", odds: 8, result: "win" }
   ],
 
   lastUpdated: "17 Jan 2026"
